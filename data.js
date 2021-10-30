0ae3d31650f07ded71a0340380462ad4618b08d1
Edward <edward@quahk.com>
2021-10-30 16:22:07 +0800

    initial commit

---
+++ data.js
@@ -0,0 +1,22 @@
+const quotes = [
+    {
+        content: "There is nothing permanent except change.",
+        author: "Heraclitus"
+    },
+    {
+        content: "No man ever steps in the same river twice, for it’s not the same river and he’s not the same man.",
+        author: "Heraclitus"
+    },
+    {
+        content: "Don’t cry because it’s over, smile because it happened.",
+        author: "Dr. Seuss"
+    },
+    {
+        content: "Two things are infinite: the universe and human stupidity; and I’m not sure about the universe.",
+        author: "Albert Einstein"
+    },
+    {
+        content: "In three words I can sum up everything I’ve learned about life: it goes on.",
+        author: "Robert Frost"
+    }
+]
